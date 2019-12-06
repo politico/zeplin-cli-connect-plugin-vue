8db76cf89d3bbdd91cca7673475c7c1521e77833
Jack Koppa <hello@jackkoppa.com>
2019-12-06 15:07:34 -0500

    fix: Remove extra snippet spacing; switch to JSX highlighting temporarily TODO: We would like to mark the snippets as HTML (while not fully accurate, it's more accurate than JSX); however, Zeplin does not yet seem to support HTML syntax highlighting

--- src/index.ts
+++ src/index.ts
@@ -6,41 +6,33 @@ export default class implements ConnectPlugin {
     supportedFileExtensions = [".vue"]
 
     supports(context: ComponentConfig): boolean {
-        // Here goes your implementation
-
-        /**
-        * CLI invokes this method per component in .zeplin/components.json file
-        * to determine if this plugin should process the component config
-        */
-
        const fileExtension = path.extname(context.path);
 
        return this.supportedFileExtensions.includes(fileExtension);
     }
 
     async process(context: ComponentConfig): Promise<ComponentData> {
-        // Here goes your implementation
-
-        /**
-        * CLI invokes this method per component in .zeplin/components.json file
-        * and returns a description, snippet, PrismLang language info for syntax highlighting
+       /** 
+        * @TODO Determine if we can switch to `PrismLang.HTML`
+        * Vue templates have no perfect analog in Prism-supported languages,
+        * though they're more similar to HTML than JSX. 
+        * However, Zeplin currently does not seem to provide syntax 
         */
-
+       const lang = PrismLang.ReactJSX
+       
        const componentInfo = await parse(context.path)
-       const lang = PrismLang.HTML
+       
        const description = componentInfo.description
-
-       console.log(componentInfo)
     
        const propsBlock = componentInfo.props && componentInfo.props.map(prop => {
            return `${prop.name}="${prop.type && prop.type.name || 'unknown'}"`
        }) || []
-       /** @TODO Replace with real, PUG-based snippet generation */
-       const snippet = `
-<${componentInfo.displayName}
+
+       /** @TODO Determine if we need pug or other templating tools to help with things like slots & events  */
+       const snippet = 
+`<${componentInfo.displayName}
   ${propsBlock.join('\n  ')}
-/>
-        `
+/>`
        return { description, snippet, lang };
     }
 }
