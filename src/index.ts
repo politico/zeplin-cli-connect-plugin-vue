35e82b3c7c161473ec984c16223bab8177947875
Jack Koppa <hello@jackkoppa.com>
2019-12-06 14:19:12 -0500

    feature: Enable real, initial generation of snippets from .vue files TODO: Plenty of work, possibly with .pug files, to include information about slots & possibly events

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
-import { ConnectPlugin, ComponentConfig, ComponentData, PrismLang } from "@zeplin/cli";
+import { ConnectPlugin, ComponentConfig, ComponentData, PrismLang } from "@zeplin/cli"
+import { parse } from 'vue-docgen-api'
+import path from "path";
 
 export default class implements ConnectPlugin {
+    supportedFileExtensions = [".vue"]
 
     supports(context: ComponentConfig): boolean {
         // Here goes your implementation
@@ -9,6 +12,10 @@ export default class implements ConnectPlugin {
         * CLI invokes this method per component in .zeplin/components.json file
         * to determine if this plugin should process the component config
         */
+
+       const fileExtension = path.extname(context.path);
+
+       return this.supportedFileExtensions.includes(fileExtension);
     }
 
     async process(context: ComponentConfig): Promise<ComponentData> {
@@ -19,5 +26,21 @@ export default class implements ConnectPlugin {
         * and returns a description, snippet, PrismLang language info for syntax highlighting
         */
 
+       const componentInfo = await parse(context.path)
+       const lang = PrismLang.HTML
+       const description = componentInfo.description
+
+       console.log(componentInfo)
+    
+       const propsBlock = componentInfo.props && componentInfo.props.map(prop => {
+           return `${prop.name}="${prop.type && prop.type.name || 'unknown'}"`
+       }) || []
+       /** @TODO Replace with real, PUG-based snippet generation */
+       const snippet = `
+<${componentInfo.displayName}
+  ${propsBlock.join('\n  ')}
+/>
+        `
+       return { description, snippet, lang };
     }
 }
