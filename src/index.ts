4fb460c9f630e3186ac306d839dd0526dbe8f50a
Jack Koppa <hello@jackkoppa.com>
2020-01-06 16:11:46 -0500

    feature (#3): Implement parsing for events & slots; improve formatting for all cases All known 'external' options of Vue components now supported (props, events, slots), with appropriate formatting for various cases (e.g. only props, only slots, props + events, props + events + slots, etc.) Going forward, tweaks to exported, parsed code will hopefully center around preference discussions & anything missed from Vue's styleguide In the future, there may be a need for configurable CLI plugins, if certain teams have certain preferences, though that seems fairly distant

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { ConnectPlugin, ComponentConfig, ComponentData, PrismLang } from "@zeplin/cli"
-import { parse } from 'vue-docgen-api'
+import { paramCase } from "param-case";
+import { pascalCase } from "pascal-case";
+import { parse, ComponentDoc } from 'vue-docgen-api'
 import path from "path";
 
 export default class implements ConnectPlugin {
@@ -16,17 +18,80 @@ export default class implements ConnectPlugin {
        
        const componentInfo = await parse(context.path)
        
-       const description = componentInfo.description
+       const { description, displayName } = componentInfo
     
-       const propsBlock = componentInfo.props && componentInfo.props.map(prop => {
-           return `${prop.name}="${prop.type && prop.type.name || 'unknown'}"`
-       }) || []
+       const propLines = this.getPropLines(componentInfo)
 
-       /** @TODO Determine if we need pug or other templating tools to help with things like slots & events  */
-       const snippet = 
-`<${componentInfo.displayName}
-  ${propsBlock.join('\n  ')}
-/>`
+       const eventLines = this.getEventLines(componentInfo)
+
+       const slotLines = this.getSlotLines(componentInfo)
+
+       const snippet = this.generateSnippet(displayName, propLines, eventLines, slotLines)
+
        return { description, snippet, lang };
+    }
+
+    getPropLines(componentInfo: ComponentDoc): string[] {
+      return componentInfo.props && componentInfo.props.map(prop => 
+        `${paramCase(prop.name)}="${prop.type && prop.type.name || 'unknown'}"`
+    ) || []
+    }
+    
+    getSlotLines(componentInfo: ComponentDoc): string[] {
+      return componentInfo.slots && componentInfo.slots.map(slot => {
+        if (slot.name === 'default') return `<!-- content -->`
+
+        if (slot.scoped) {
+          let slotBinding = 'scopedBinding'
+          if (slot.bindings && slot.bindings[0] && slot.bindings[0].name) slotBinding = slot.bindings[0].name
+          return `<template v-slot:${slot.name}="${slotBinding}"></template>`
+        }
+
+        return `<template v-slot:${slot.name}></template>`
+      }) || []
+    }
+
+    getEventLines(componentInfo: ComponentDoc): string[] {
+      return componentInfo.events && componentInfo.events.map(event => {
+        console.log(JSON.stringify(event))
+        return `@${event.name}="handle${pascalCase(event.name)}"`
+      }) || []
+    }
+
+    generateSnippet(displayName: string, propLines: string[], eventLines: string[], slotLines: string[]): string {
+      let snippet =''
+
+      const combinedPropEventLines = [...propLines, ...eventLines]
+
+      if (!combinedPropEventLines.length && !slotLines.length) {
+
+        snippet = `<${displayName} />`
+
+      } else if (combinedPropEventLines.length && !slotLines.length) {
+
+        snippet =
+`<${displayName}
+  ${combinedPropEventLines.join('\n  ')}
+/>`
+
+      } else if (!combinedPropEventLines.length && slotLines.length) {
+
+        snippet =
+`<${displayName}>
+  ${slotLines.join('\n  ')}
+</${displayName}>`
+
+      } else {
+
+        snippet =
+`<${displayName}
+  ${combinedPropEventLines.join('\n  ')}
+>
+  ${slotLines.join('\n  ')}
+</${displayName}>`
+
+      } 
+
+      return snippet
     }
 }
